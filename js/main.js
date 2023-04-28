4f2069774126d5b546ece12d99bad9cf4933008a
firefoxic <firefoxic.dev@gmail.com>
2023-04-28 06:18:12 +0000

    Deploying to gh-pages from @ htmlacademy-adaptive/2098045-cat-energy-27@b966be649d39dfe54d0ba8a9be304a0ee4ae9f37 🚀

---
+++ js/main.js
@@ -0,0 +1,1 @@
+let mainNav=document.querySelector(".main-nav"),navToggle=document.querySelector(".main-nav__toogle");mainNav.classList.remove("main-nav--nojs"),mainNav.classList.add("main-nav--closed"),navToggle.addEventListener("click",(function(){mainNav.classList.toggle("main-nav--closed")}));
